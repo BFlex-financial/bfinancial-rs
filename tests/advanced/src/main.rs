38de5c96d81c2ea697d481adafa43f74d00a8ade
lucasFelixSilveira <lucasdwbfff@gmail.com>
2024-08-29 22:52:39 -0300

    test file modified

--- tests/advanced/src/main.rs
+++ tests/advanced/src/main.rs
@@ -1,45 +1,50 @@
-use bfinancial_rs;
-use bfinancial_rs::models::client::payment;
 use tokio;
+use bfinancial_rs::{ Client, models::client::payment::{self, PaymentCreate} };
 
 async fn test_pix(){
-    let client = bfinancial_rs::Client::login("admin");
+    let client = Client::login("admin");
     let payments = client.payments;
-    let payment_data = payments.create(payment::PaymentCreate::Pix(payment::PixCreate {
-        amount: 0.2,
+    let payment_data = payments.create(PaymentCreate::Pix(payment::PixCreate {
+        // Payment amount
+        amount: 22.0,
+
+        // Payer information
         payer_email: "test@gmail.com".into()
     })).await;
 
     match payment_data {
         Ok(pix) => println!("{:#?}", pix),
-        Err(falha) => println!("Falhou em {}", falha)
+        Err(fail) => println!("Error returned when generating payment: {}", fail)
     }
 }
+
 async fn test_card(){
-    let client = bfinancial_rs::Client::login("admin");
+    let client = Client::login("admin");
     let payments = client.payments;
-    let payment_data = payments.create(payment::PaymentCreate::Card(payment::CardCreate {
+    let payment_data = payments.create(PaymentCreate::Card(payment::CardCreate {
+        // Payment amount
         amount: 22.0,
+
+        // Payer informations
         payer_email: "test@gmail.com".into(),
         payer_cpf: "12345678909".into(),
-        payer_name: "CONT".into(),
+        payer_name: "test user".into(),
+
+        // Card information
         expiration_month: 11,
         expiration_year: 2025,
-        number:"5031433215406351".into(),
-        cvv:"123".into()
+        number: "5031433215406351".into(),
+        cvv: "123".into()
     })).await;
 
     match payment_data {
         Ok(pix) => println!("{:#?}", pix),
-        Err(falha) => println!("Falhou em {}", falha)
+        Err(fail) => println!("Error returned when generating payment: {}", fail)
     }
 }
 
-
-
-
 #[tokio::main]
 async fn main() {
-    // test_pix().await;
+    test_pix().await;
     test_card().await;
 }
